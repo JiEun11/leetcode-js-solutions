007b6e9848de4f9cff71a0a984fbbd3284b01ae2
Bella Jin <35979424+JiEun11@users.noreply.github.com>
2026-07-10 00:31:02 +0900

    Time: 1 ms (87.74%), Space: 55.3 MB (41.42%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const visitedNumbers = new Map(); // key: 숫자값, value: 인덱스
+    const seen = new Map(); // key: number, value: nums index
+    for (let index = 0; index < nums.length; index++ ) {
+        const complement = target - nums[index];
 
-    for (let index = 0; index < nums.length; index++) {
-        const currentNumber = nums[index];
-        const complement = target - currentNumber // 1)target에서 뭘 빼야 할까요?
-
-        /* 2) complement가 Map에 있는지 확인 */
-        if (visitedNumbers.has(complement)) {
-            /* 3) complement의 인덱스 */
-            return [visitedNumbers.get(complement), index];
+        if (seen.has(complement)) {
+            return [seen.get(complement), index];
         }
-
-        // 4) 없으면 현재 숫자를 Map에 저장 (무엇을 key, value로?)
-        visitedNumbers.set(currentNumber, index);
+        seen.set(nums[index], index);
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
