44a6eaee6c29509aa88636286f0b66faa61fef47
Bella Jin <35979424+JiEun11@users.noreply.github.com>
2026-07-26 22:38:09 +0900

    Time: 4 ms (53.32%), Space: 54.8 MB (56.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,17 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const seen = new Map(); // key: number, value: nums index
-    for (let index = 0; index < nums.length; index++ ) {
-        const complement = target - nums[index];
+    const seen = new Map();
 
-        if (seen.has(complement)) {
-            return [seen.get(complement), index];
-        }
-        seen.set(nums[index], index);
+    for (let i = 0; i < nums.length; i++) {
+       const current = nums[i];
+       const complement = target - current;
+
+        // Check whether we have already seen the complement.
+       if (seen.has(complement)) {
+        return [seen.get(complement), i];
+       }
+
+        // Store the current number and its index.
+        seen.set(current, i);
     }
 };
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
